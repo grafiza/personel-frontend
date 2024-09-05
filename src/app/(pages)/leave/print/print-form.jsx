46c531e103add3579cc65509edbeeca303c47412
Zafer Kanbur <zaferkanbur@hotmail.com>
2024-09-05 17:13:44 +0300

    "v3"

--- src/app/(pages)/leave/print/print-form.jsx
+++ src/app/(pages)/leave/print/print-form.jsx
@@ -19,57 +19,57 @@ const PrintForm = ({ getLeave }) => {
             <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5" ref={componentRef}> {/* ref'i buraya ekleyin */}
                 <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                     <div className="overflow-hidden">
-                        <div className='mt-5 p-5 text-center text-xl'>
-                            <p>FİRMA ÜNVANI: DİNLER ÇİVİ BOYA İNŞ. GIDA PAZ.LTD.ŞTİ</p>
-                            <p>ADRES : SANCAKLAR MAH. AKÇAKOCA KARAYOLU CAD. NO:111 MERKEZ / DÜZCE</p>
+                        <div className='mt-5 p-5 text-center text-2xl'>
+                            <p className='p-4'>FİRMA ÜNVANI: DİNLER ÇİVİ BOYA İNŞ. GIDA PAZ.LTD.ŞTİ</p>
+                            <p className='p-4'>ADRES : SANCAKLAR MAH. AKÇAKOCA KARAYOLU CAD. NO:111 MERKEZ / DÜZCE</p>
                         </div>
-                        <table className="table-auto">
+                        <table className="table-auto text-lg">
                             <thead>
                                 <tr>
-                                    <th scope="col" colSpan="2" className="text-3xl font-bold py-10 text-center">
+                                    <th scope="col" colSpan="2" className="text-4xl font-bold py-10 text-center">
                                         <u>PERSONEL İZİN İSTEK FORMU</u>
                                     </th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr className='border-b'>
-                                    <td className="px-6 py-2" width="40%">İzin Düzenleme Tarihi</td>
-                                    <td className="px-6 py-2">{formatDate(today)}</td>
+                                    <td className="px-6 py-3" width="40%">İzin Düzenleme Tarihi</td>
+                                    <td className="px-6 py-3">{formatDate(today)}</td>
                                 </tr>
                                 <tr className='border-b'>
-                                    <td className="px-6 py-2">Adı Soyadı</td>
-                                    <td className="px-6 py-2">{getLeave.employee.firstName} {getLeave.employee.lastName}</td>
+                                    <td className="px-6 py-3">Adı Soyadı</td>
+                                    <td className="px-6 py-3">{getLeave.employee.firstName} {getLeave.employee.lastName}</td>
                                 </tr>
                                 <tr className='border-b'>
-                                    <td className="px-6 py-2">İzin Başlangıç Tarihi</td>
-                                    <td className="px-6 py-2">{formatDate(getLeave.leaveStartDate)}</td>
+                                    <td className="px-6 py-3">İzin Başlangıç Tarihi</td>
+                                    <td className="px-6 py-3">{formatDate(getLeave.leaveStartDate)}</td>
                                 </tr>
                                 <tr className='border-b'>
-                                    <td className="px-6 py-2">İzin Bitiş Tarihi</td>
-                                    <td className="px-6 py-2">{formatDate(getLeave.leaveEndDate)}</td>
+                                    <td className="px-6 py-3">İzin Bitiş Tarihi</td>
+                                    <td className="px-6 py-3">{formatDate(getLeave.leaveEndDate)}</td>
                                 </tr>
                                 <tr className='border-b'>
-                                    <td className="px-6 py-2">İzin Türü</td>
-                                    <td className="px-6 py-2">{leaveTypes(getLeave.leaveType)}</td>
+                                    <td className="px-6 py-3">İzin Türü</td>
+                                    <td className="px-6 py-3">{leaveTypes(getLeave.leaveType)}</td>
                                 </tr>
                                 <tr className='border-b'>
-                                    <td className="px-6 py-2">İzin Süresi</td>
-                                    <td className="px-6 py-2">{leaveTypes(getLeave.leaveDays)}</td>
+                                    <td className="px-6 py-3">İzin Süresi</td>
+                                    <td className="px-6 py-3">{leaveTypes(getLeave.leaveDays)}</td>
                                 </tr>
                                 <tr className='border-b'>
-                                    <td className="px-6 py-2">Kalan İzin Süresi</td>
-                                    <td className="px-6 py-2">{leaveTypes(getLeave.employee.remainingLeaveDays)}</td>
+                                    <td className="px-6 py-3">Kalan İzin Süresi</td>
+                                    <td className="px-6 py-3">{leaveTypes(getLeave.employee.remainingLeaveDays)}</td>
                                 </tr>
                                 <tr className='border-b h-32'>
-                                    <td className="px-6 py-2">İmza</td>
-                                    <td className="px-6 py-2"></td>
+                                    <td className="px-6 py-3">İmza</td>
+                                    <td className="px-6 py-3"></td>
                                 </tr>
                                 <tr className='border-b'>
                                     <td colSpan="2" className='p-10'>
-                                        Bu izin yukarıda bahsedilen maksat için kullanılmadığı veya izin süresine uyulmadığı taktirde ilgili mevzuata göre cezalandırılacaktır.
+                                        Bu izin yukarıda bahsedilen maksat için kullanılmadığı veya izin süresine uyulmadığı taktirde ilgili mevzuata göre cezai işlem uygulanacaktır.
                                     </td>
                                 </tr>
-                                <tr className='border-b h-72 align-text-top text-center'>
+                                <tr className='border-b h-36 align-text-top text-center'>
                                     <td>BÖLÜM SORUMLUSU</td>
                                     <td>KAŞE / İMZA</td>
                                 </tr>
